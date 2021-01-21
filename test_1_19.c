66e401a28e016e8c8fbc15a1560f8165a28cbc6b
柏诗成 <1262430186@qq.com>
2021-01-21 18:31:34 +0800

    1-21

---
+++ test_1_19.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+
+//#include <stdio.h>
+//int main()
+//{
+//	char al = 0;
+//	while ((scanf("%c",&al)) != EOF)
+//	{
+//		if ((al >= 'A') && (al <= 'Z'))
+//		{
+//			al = al + 32;
+//			putchar(al);
+//			printf("\n");
+//		}
+//		else if ((al >= 'a') && (al <= 'z'))
+//		{
+//			al = al - 32;
+//			putchar(al);
+//			printf("\n");
+//		}
+//	}
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char al = 0;
+//	while ((al=getchar()) != EOF)
+//	{
+//		if (al == '\n')
+//		{
+//			continue;
+//		}
+//		if ((al>='A') && (al<='Z'))
+//		{
+//			al = al + 32;
+//			printf("%c\n",al);
+//		}
+//		else if ((al>='a') && (al<='z'))
+//		{
+//			al = al - 32;
+//			printf("%c\n",al);
+//		}
+//	}
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char al = 0;
+//	while ((al = getchar()) != EOF)
+//	{
+//		if (al == '\n')
+//		{
+//			continue;
+//		}
+//		if ((al >= 'A') && (al <= 'Z'))
+//		{
+//			al = al + 32;
+//			printf("%c\n", al);
+//		}
+//		else if ((al >= 'a') && (al <= 'z'))
+//		{
+//			al = al - 32;
+//			printf("%c\n", al);
+//		}
+//	}
+//}
