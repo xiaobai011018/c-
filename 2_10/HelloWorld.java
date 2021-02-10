6d60c1ddb06f2f2948f917fa62f45544bf4ebd87
柏诗成 <1262430186@qq.com>
2021-02-10 22:13:11 +0800

    2-10

---
+++ 2_10/HelloWorld.java
@@ -0,0 +1,84 @@
+public class HelloWorld {
+	public static final double CM_PER_INCH = 2.5;
+	public static void main(String[] args) {
+		double paperWidth = 8.5;
+		double paperHeight = 11;
+		System.out.println("paper size in centimeters:" +
+			paperWidth +
+			CM_PER_INCH +
+			"by" +
+			paperHeight +
+			CM_PER_INCH);
+	}
+	public static void main4(String[] args) {
+		double salary = 65000.0;
+		System.out.println(salary);
+		int vacationDays = 12;
+	}
+	public static void main3(String[] args) {
+		final double CM_PER_INCH = 2.54;
+		double paperWidth = 8.5;
+		double paperHeight = 11;
+		System.out.println("Paper size in centimeters:"
+		 + paperWidth 
+		 + CM_PER_INCH 
+		 + "by"
+		 + paperHeight +CM_PER_INCH);
+	}
+	public static void main2(String[] args) {
+		System.out.print("HelloWorld");
+		/*System.out.println();*/
+		System.out.println("we will not use 'Hello.World!'");
+	}
+	public static void main1(String[] args) {
+		String str = "100";
+		int num = Integer.parseInt(str);
+		System.out.println(str);
+	}
+}
+	/**
+	 * 
+	 */
+	/**
+	 * [main description]
+	 * @param args [description]
+	 */
+/*	public static void main(String[] args) {
+		String str = "result=";
+		int a = 10;
+		int b = 20;
+		String result = str + a + b;
+		System.out.println(result);
+	}*/
+/*	public static void main(String[] args) {
+		int num = 10;
+		String str1 = num + "";
+		System.out.println(num);
+	}*/
+	/*public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println("a= "+ a + ",b = " + b);
+	}*/
+/*	public static void main(String[] args) {
+		string str = "result=";
+		int a = 10;
+		int b = 20;
+		string result = str + a + b;
+		System.out.println(result);
+	}*/
+
+/*	public static void main2(String[] args) {
+		int a = 5;
+		System.out.println(a);
+	}
+	public static void main1(String[] args) {
+		int b = 4;
+		System.out.println(b);
+	}*/
+/*public class HelloWorld {
+	public static void main1(String[] args) {
+		int a = 9;
+		System.out.println("a的值是：" + a);
+	
+}*/
