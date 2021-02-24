8f6f45e408aa52ce0a8f525541c37c7cf95cfe4a
柏诗成 <1262430186@qq.com>
2021-02-24 22:34:39 +0800

    2-24

---
+++ 20210224/src/TestDemo.java
@@ -0,0 +1,27 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        TestJava now = new TestJava();
+        now.addFirst(10);
+        now.addFirst(10);
+        now.addFirst(12);
+        now.addFirst(10);
+        now.addFirst(13);
+        now.addFirst(13);
+        now.display();
+        //System.out.println(now.SearchMid().data);
+        System.out.println(now.searchLastKey(5).data);
+
+        //now.display();
+//        now.display();
+//        System.out.println(now.contains(11));
+//        System.out.println(now.size());
+//        now.addIndex(4,4);
+//        now.display();
+//        now.remove(10);
+//        now.remove(1);
+//        now.removeAllKey(10);
+//        Node ret = now.RoateList();
+//        now.display2(ret);
+
+    }
+}
