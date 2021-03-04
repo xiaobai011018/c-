58c2cca11684a0f2d8c81d0352ebd535a7ccae37
柏诗成 <1262430186@qq.com>
2021-03-04 21:50:33 +0800

    3-4

---
+++ 20210304/src/TestJava.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class TestJava {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()) {
+            int n = scanner.nextInt();
+            System.out.println(n);
+        }
+    }
+}
