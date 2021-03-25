c9b222d82e87c4ea1a4ba0127e6478f5ae0f9e45
柏诗成 <1262430186@qq.com>
2021-03-25 23:23:53 +0800

    3-25

---
+++ 03_25/src/TestJava.java
@@ -0,0 +1,18 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class TestJava {
+   static final int a = 3;
+    enum Size {SMALL,MEDIUM,LARGE,EXTRA};
+    public static void main(String[] args) {
+       Size sz = Size.SMALL;
+        //System.out.printf("%d",sz);
+    }
+    public static void main1(String[] args) {
+        final int arr = 1;
+        TestJava java = new TestJava();
+        int i = 1;
+        System.out.println(i);
+        System.out.println(a);
+
+    }
+}
