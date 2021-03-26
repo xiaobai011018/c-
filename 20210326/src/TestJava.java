b480b41d4c99278773b5acd9af73d47c2ce42b45
柏诗成 <1262430186@qq.com>
2021-03-26 23:02:52 +0800

    3-26

---
+++ 20210326/src/TestJava.java
@@ -0,0 +1,16 @@
+import static java.lang.Math.*;
+public class TestJava {
+    public static void main(String[] args) {
+        String arr = "abcd";
+        String arr1 = arr.substring(1,3);
+        System.out.println(arr1);
+    }
+    public static void main2(String[] args) {
+        int a = (int)pow(3,2);
+        System.out.println(a);
+    }
+    public static void main1(String[] args) {
+        int a = 3;
+        System.out.println(sqrt(a));
+    }
+}
