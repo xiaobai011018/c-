d22d1b57d244e8f8d936c029b4603d1b58a8e992
柏诗成 <1262430186@qq.com>
2021-03-31 23:24:59 +0800

    3-31

---
+++ 20210331/src/TestDemo.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        LocalDate newYear = new LocalDate();
+        int year = LocalDate.
+        System.out.println(LocalDate.now().toString());
+        System.out.println(new Date().toString());
+    }
+}
