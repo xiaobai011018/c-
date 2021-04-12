cbfd2fd5455c378b3ed47878c707fc793b8bc268
柏诗成 <1262430186@qq.com>
2021-04-12 23:54:37 +0800

    4-12

---
+++ 20210412/src/TestDemo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class TestDemo {
+    @SuppressWarnings({"all"})
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("avc");
+        arrayList.add(123);
+        System.out.println("List=" + arrayList);
+
+    }
+}
