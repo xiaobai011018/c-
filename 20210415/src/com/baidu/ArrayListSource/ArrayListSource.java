6a653a1ba05da8c8a272bfc31dbb354438e1992a
柏诗成 <1262430186@qq.com>
2021-04-15 23:27:31 +0800

    4-15

---
+++ 20210415/src/com/baidu/ArrayListSource/ArrayListSource.java
@@ -0,0 +1,18 @@
+package com.baidu.ArrayListSource;
+
+import java.util.ArrayList;
+@SuppressWarnings({"all"})
+public class ArrayListSource {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        for (int i = 1; i <= 10; i++) {
+            arrayList.add(i);
+        }
+        for (int i = 11; i <= 15; i++) {
+            arrayList.add(i);
+        }
+        arrayList.add(100);
+        arrayList.add(120);
+
+    }
+}
---
+++ 20210415/src/com/baidu/ArrayListSource/LinkedListedCRUD.java
@@ -0,0 +1,12 @@
+package com.baidu.ArrayListSource;
+
+import java.util.LinkedList;
+@SuppressWarnings({"all"})
+public class LinkedListedCRUD {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(1);
+        linkedList.add(2);
+
+    }
+}
