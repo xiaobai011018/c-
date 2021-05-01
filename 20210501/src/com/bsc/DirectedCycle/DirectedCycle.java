eda43160158dc5f6ab0c3a3fcff0aaf508b49a94
柏诗成 <1262430186@qq.com>
2021-05-01 21:58:56 +0800

    5-1

---
+++ 20210501/src/com/bsc/DirectedCycle/DirectedCycle.java
@@ -0,0 +1,16 @@
+package com.bsc.DirectedCycle;
+
+import java.util.Stack;
+
+public class DirectedCycle {
+    private boolean[] marked;
+    private boolean hasCycle;
+    private boolean[] onStack;
+
+    public static void main(String[] args) {
+        Stack<Integer> intergers = new Stack<Integer>();
+    }
+    public DirectedCycle(boolean[] marked) {
+        this.marked = marked;
+    }
+}
